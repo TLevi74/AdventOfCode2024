89d401c23fb3f7e76a3f0d340e988496550074ce
TLevi74 <152499824+TLevi74@users.noreply.github.com>
2024-12-03 18:17:44 +0100

    aoc day 3

---
+++ aoc3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace aoc3
+{
+    internal class aoc3
+    {
+        static void Main(string[] args)
+        {
+            //part 1
+            string input = System.IO.File.ReadAllText("input.txt");
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    string parttofind = "mul(" + i.ToString() + "," + j.ToString() + ")";
+                    sum += (Regex.Matches(input, Regex.Escape(parttofind)).Count * i * j);
+                }
+            }
+            Console.WriteLine(sum);
+
+            //part 2
+            sum = 0;
+
+            //!!!!
+            string pattern = @"don't\(\).*?do\(\)";
+            input = Regex.Replace(input, pattern, "", RegexOptions.Singleline);
+            //!!!!
+
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    string parttofind = "mul(" + i.ToString() + "," + j.ToString() + ")";
+                    sum += (Regex.Matches(input, Regex.Escape(parttofind)).Count * i * j);
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
